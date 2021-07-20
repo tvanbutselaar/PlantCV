4b265a615076b8efb251dc52ede370a40740d4d5
Butselaar <t.vanbutselaar@uu.nl>
2021-07-20 12:03:05 +0200

    New version
    Compatibility with PlantCV v3.12.1, older versions now unsupported.
    Variables moved from .sh to notes.txt file

--- plantcv_master.sh
+++ plantcv_master.sh
@@ -1,7 +1,9 @@
 ###
 # @author: tvanbutselaar
-# Current version: 20210715
-# Changes: HPC compatibility
+# Current version: 20210720
+# Changes: HPC compatibility, variables.txt now moved to notes.txt
+
+# This version compatible with PlantCV v3.12.1 (and possibly later)
 
 # Working script for PlantCV image analysis tool to measure rosette area in multiplant pictures. Information on https://plantcv.readthedocs.io/. This script encompasses 5 EOF files that form the main functions, and a final for loop that calls the first EOF bash script for all pictures in input folder.
 
@@ -23,37 +25,21 @@
 
 # Subsequent analysis will prep a figure with bars for every category in the experiment. This part is dependent on a notes.txt file, which should contain: 
 	# picture names and their corresponding genotypes (also include a stylized genotype if you want), this all tab-separated. The stylized genotype should be the last part of this line. Each picture on new line. 
-	# a line describing the squared pixel to squared mm ratio. the line must contain the word 'Pixel ratio' (Case-sensitive) and must be ending with a colon followed by the squared pixel to squared mm ratio.
-		# Protips: 
+			# Protips: 
 			# flank your text with $ if you want the text to be printed in italics in the plot
 			# use mathregular-based expressions if you want to use subscript, superscript, or special characters in the plot: https://matplotlib.org/3.3.3/tutorials/text/mathtext.html
-		
-# Use a representative picture and analyze in Photoshop to estimate the parameters in the variable list below. These parameters will be called on in the image analysis pipeline. These are the primary parameters that need to be adjusted for your images. The main variables to be altered are those for ROI, Cluster Nrow/Ncol, Rotation, and Shift numbers. Run the program locally and use output from the first image to further estimate other parameters
+	# all variables for this script to run separated on newlines
 
+
+# Before use, update the notes.txt file to optimized parameters for your batch of pictures. Use a representative picture and analyze in Photoshop to estimate the parameters in the variable list. 
 ###
 
 mpwd=$(pwd)
 
-#Use a representative picture and analyze in Photoshop to estimate the parameters in the variable list below. These parameters will be called on in the image analysis pipeline. These are the primary parameters that need to be adjusted for your images. The main variables to be altered are those for ROI, Cluster Nrow/Ncol, Rotation, and Shift numbers. Run the program locally and use output from the first image to further estimate other parameters
 
-cat <<\EOF > "$mpwd"/$2_variables.txt
-0 ##			Script 1+2 ROI x_adj			the top-left pixel defining the rectangle of ROI
-0 ##			Script 1+2	ROI y_adj			the top-left pixel defining the rectangle of ROI
-1700 ##	Script 1+2	ROI h_adj			value for how far along y-axis from top-left pixel the ROI stretches
-3400 ##	Script 1+2	ROI w_adj			value for how far along x-axis from top-left pixel the ROI stretches
-4 ##		Script 1		cluster nrow		value for number of rows of cluster. if plants parts are identified okay, but are not declustered correctly, this is the main parameter to tweak
-6 ##		Script 1		cluster ncol		value for number of columns to cluster. if plants parts are identified okay, but are not declustered correctly, this is the main parameter to tweak
--0.3 ##		Script 1		rotation_deg		rotation angle in degrees, can be a negative number, positive values move counter clockwise
-100 ##		Script 1		shift number		value for shifting image up x pixels, must be greater than 1
-150 ##		Script 1		shift number		value for shifting image left x pixels, must be greater than 1
-120 ##		Script 1		bin treshold		threshold value (0-255) for binary tresholding, higher values will generate more background. if missing a lot of plant parts, or you have too much background, this is the main parameter to tweak
-200 ##		Script 1		fill size				minimum pixel size for objects, those smaller will be filled
-1 ##			Script 1		dilation kernel 	an odd integer that is used to build a ksize x ksize matrix using np.ones. Must be greater than 1 to have an effect. Greater values will ensure all plant parts are included, but also will overestimate size of plant
-1 ##			Script 1		dilation iter		number of consecutive filtering passes for dilation. Greater values will ensure all plant parts are included, but also will overestimate size of plant
-EOF
 
 #Generate main python script for declustering pictures
-cat <<\EOF > "$mpwd"/$2_python1.py
+cat <<\EOF > "$mpwd"/"$2"_python1.py
 #!/usr/bin/python
 
 import warnings
@@ -85,16 +71,41 @@ def options():
 def main(img8,tray, args, pwd):
 	# Get options
 	args = options()
-	if args.names!= None:
-		args.names="../../names/%s.txt" %(str(tray))
-		print("Names file: %s" %(args.names))
-	else:
-		print("No names file submitted for %s" %(str(tray)))
+	# if args.names!= None:
+		# args.names="../../names/%s.txt" %(str(tray))
+		# print("Names file: %s" %(args.names))
+	# else:
+		# print("No names file submitted for %s" %(str(tray)))
 		
 	#Get variables from variable arguments
-	with open(args.variables) as f:
-		roi1=[line.split(' ')[0] for line in f]
-
+	for line in open(args.variables, 'r').readlines():
+		if line.startswith("shift_up"):
+			shift_up=int(line.strip().split(":")[-1])
+		if line.startswith("shift_left"):
+			shift_left=int(line.strip().split(":")[-1])
+		if line.startswith("ROI_x_adj"):
+			ROI_x_adj=int(line.strip().split(":")[-1])
+		if line.startswith("ROI_y_adj"):
+			ROI_y_adj=int(line.strip().split(":")[-1])
+		if line.startswith("ROI_h_adj"):
+			ROI_h_adj=int(line.strip().split(":")[-1])
+		if line.startswith("ROI_w_adj"):
+			ROI_w_adj=int(line.strip().split(":")[-1])
+		if line.startswith("rotation_deg"):
+			rotation_deg=float(line.strip().split(":")[-1])
+		if line.startswith("cluster_nrow"):
+			cluster_nrow=int(line.strip().split(":")[-1])
+		if line.startswith("cluster_ncol"):
+			cluster_ncol=int(line.strip().split(":")[-1])
+		if line.startswith("bin_tresh"):
+			bin_tresh=int(line.strip().split(":")[-1])
+		if line.startswith("fill_size"):
+			fill_size=int(line.strip().split(":")[-1])
+		if line.startswith("dilation_kernel"):
+			dilation_kernel=int(line.strip().split(":")[-1])
+		if line.startswith("dilation_iter"):
+			dilation_iter=int(line.strip().split(":")[-1])
+		
 	# Read image
 	img, path, filename = pcv.readimage(img8)
 	pcv.params.debug=args.debug #set debug mode
@@ -103,25 +114,34 @@ def main(img8,tray, args, pwd):
 	img1 = pcv.white_balance(img,roi=None)
 	
 	## STEP 3: Rotate the image
-	rotate_img = pcv.rotate(img1, float(roi1[6]), True)
+	if rotation_deg != 0:
+		rotate_img = pcv.transform.rotate(img1, float(rotation_deg), True)
+	else:
+		rotate_img = img1
 	
 	## STEP 4: Shift image. This step is important for clustering later on. The resulting image is the same size as the original.
-	shift1 = pcv.shift_img(rotate_img, int(roi1[7]), "bottom")
-	shift2 = pcv.shift_img(shift1, int(roi1[8]), "right")
-	img1 = shift2
+	if shift_up > 0:
+		shift1=pcv.shift_img(rotate_img, shift_up, "bottom")
+	else:
+		shift1=rotate_img
+	if shift_left > 0:
+		shift2=pcv.shift_img(shift1, shift_left, "right")
+		img1=shift2
+	else:
+		img1=shift1
 
 	## STEP 5: Convert image from RGB colorspace to LAB colorspace. Keep only the green-magenta channel (grayscale).
 	a = pcv.rgb2gray_lab(img1, "a")
 
 	## STEP 6: Set a binary threshold on the saturation channel image.
-	img_binary = pcv.threshold.binary(a, int(roi1[9]), 255, "dark")
+	img_binary = pcv.threshold.binary(a, bin_tresh, 255, "dark")
 
 	## STEP 7: Fill in small objects (speckles)
-	fill_image = pcv.fill(img_binary, int(roi1[10]))
+	fill_image = pcv.fill(img_binary, fill_size)
 
 	## STEP 8: Dilate so that you don't lose leaves (just in case). Only do this if dilation parameters allow this.
-	if int(roi1[11]) > 1 and int(roi1[12]) > 1:
-		dilated = pcv.dilate(fill_image, int(roi1[11]), int(roi1[12]))
+	if dilation_kernel > 1 and dilation_iter > 1:
+		dilated = pcv.dilate(fill_image, dilation_kernel, dilation_iter)
 	else:
 		print("Dilation kernal and iter <= 1, skipping dilation...")
 		dilated=fill_image
@@ -130,7 +150,7 @@ def main(img8,tray, args, pwd):
 	id_objects, obj_hierarchy = pcv.find_objects(img1, dilated)
 
 	## STEP 10: Define region of interest (ROI)
-	roi_contour, roi_hierarchy = pcv.roi.rectangle(img1, int(roi1[0]), int(roi1[1]), int(roi1[2]), int(roi1[3]))
+	roi_contour, roi_hierarchy = pcv.roi.rectangle(img1, ROI_x_adj, ROI_y_adj, ROI_h_adj, ROI_w_adj)
 
 	## STEP 11: Keep objects that overlap with the ROI
 	roi_objects, roi_obj_hierarchy, kept_mask, obj_area = pcv.roi_objects(img1, roi_contour, roi_hierarchy, id_objects, obj_hierarchy, roi_type='partial')
@@ -139,11 +159,11 @@ def main(img8,tray, args, pwd):
 	cropped = pcv.auto_crop(img1, np.vstack(id_objects), padding_x=0, padding_y=0, color='black')
 
 	## STEP 12: This function take a image with multiple contours and clusters them based on user input of rows and columns
-	clusters_i, contours, hierarchies = pcv.cluster_contours(img1, roi_objects, roi_obj_hierarchy, int(roi1[4]), int(roi1[5]), show_grid=True)
+	clusters_i, contours, hierarchies = pcv.cluster_contours(img1, roi_objects, roi_obj_hierarchy, cluster_nrow, cluster_ncol, show_grid=True)
 	
 	## STEP 13: This function takes the outputs from cluster_contours and plots on it the labels of each object. This helps for better visualisation of what objects declustered to what output
 	pcv.params.debug = "print"
-	clusterfuck = pcv.visualize.clustered_contours(img1, clusters_i, roi_objects, roi_obj_hierarchy, nrow=int(roi1[4]),ncol=int(roi1[5]))
+	clusterfuck = pcv.visualize.clustered_contours(img1, clusters_i, roi_objects, roi_obj_hierarchy, nrow=cluster_nrow,ncol=cluster_ncol)
 	listedpic=glob.glob(os.path.join(args.outdir, "*_clusters.png"))
 	latest_file=max(listedpic, key=os.path.getctime)
 	shutil.copy(latest_file, os.path.join(os.path.dirname(args.outdir), "{0}_clusters_contours.jpg".format(tray)))
@@ -168,7 +188,7 @@ main(ima, picname, args, pcwd)
 EOF
 
 #Generate second python script for output image measurements
-cat <<\EOF > "$mpwd"/$2_python2.py
+cat <<\EOF > "$mpwd"/"$2"_python2.py
 #!/usr/bin/python
 
 import warnings
@@ -202,8 +222,15 @@ def analysis(image1, binary1, plantnr, tray):
 	args = options()
 	
 	#Get variable info from variables arguments
-	with open(args.variables) as f:
-		roi1=[line.split(' ')[0] for line in f]
+	for line in open(args.variables, 'r').readlines():
+		if line.startswith("ROI_x_adj"):
+			ROI_x_adj=int(line.strip().split(":")[-1])
+		if line.startswith("ROI_y_adj"):
+			ROI_y_adj=int(line.strip().split(":")[-1])
+		if line.startswith("ROI_h_adj"):
+			ROI_h_adj=int(line.strip().split(":")[-1])
+		if line.startswith("ROI_w_adj"):
+			ROI_w_adj=int(line.strip().split(":")[-1])
 
 	# Convert images to np arrays for analysis, something I have added
 	binarpic=Image.open(binary1)
@@ -213,14 +240,14 @@ def analysis(image1, binary1, plantnr, tray):
 
 	# Find objects again within ROI, see also in main function!, from PlantCV
 	id_objects, obj_hierarchy = pcv.find_objects(ima, binar)
-	roi_contour, roi_hierarchy = pcv.roi.rectangle(ima, int(roi1[0]), int(roi1[1]), int(roi1[2]), int(roi1[3]))
+	roi_contour, roi_hierarchy = pcv.roi.rectangle(ima, ROI_x_adj, ROI_y_adj, ROI_h_adj, ROI_w_adj)
 
 	#From image inputs, generate np arrays and calculate shape area, generate error if analyze_object could fail due to vertices errors, from PlantCV
 	roi_objects, hierarchy3, kept_mask, obj_area = pcv.roi_objects(ima, roi_contour, roi_hierarchy, id_objects, obj_hierarchy, roi_type='partial')
 	obj, mask = pcv.object_composition(ima, roi_objects, hierarchy3)
 	try:
 		shape_image = pcv.analyze_object(ima, obj, mask)
-		shape_area = pcv.outputs.observations['area']['value']
+		shape_area = pcv.outputs.observations['default']['area']['value']
 		data=' '.join((tray, str(plantnr), str(shape_area)))
 		out=open(args.result, "a+")
 		out.write((data + '\n'))
@@ -245,7 +272,7 @@ analysis(imgb, imga, picname, trayname)
 EOF
 
 #And third python script to summarize all data in a figure
-cat <<\EOF > "$mpwd"/$2_python3.py
+cat <<\EOF > "$mpwd"/"$2"_python3.py
 #Post analysis script, to analyze results of plantcv output and to generate figures 
 #NOTES file should contain: 
 #    - Tab-separated picture name, genotype name, each on a new line. The order of genotypes will used as key to the order in the final graph
@@ -280,6 +307,7 @@ inres=os.path.join(mwd, "{0}.totalresults.txt".format(args.output))
 notes=os.path.join(mwd, "notes.txt")
 
 #Dependency: pairwise comparison letter generator
+
 """
 'Python pairwise comparison letter generator'
 
@@ -287,9 +315,6 @@ Github: PhilPlantMan
 """
 import string
 import random
-import statsmodels.stats.multicomp as multi
-import scikit_posthocs as sp
-
 
 def multi_comparisons_letter_df_generator(comparisons_df, letter_ordering_series = None, 
                                           primary_optimisation_parameter = "Number of different letters", 
@@ -461,7 +486,7 @@ for line in open(notes, 'r').readlines():
         if cat not in catorder:
             catorder.append(cat)
             catdict[name] = cat
-    if "Pixel ratio" in line:
+    if line.startswith("pixel_ratio"):
         pixratio=float(line.strip().split(":")[-1])
 
 # catorder=sorted(set(catorder), key=catorder.index)
@@ -537,7 +562,7 @@ print("All done. boxplot.png/.svg and statistics.txt saved to {0}".format(mwd))
 EOF
 
 #First bash script that calls Python1 and Python2 per input picture
-cat <<\EOF > "$mpwd"/$2_bash1.sh
+cat <<\EOF > "$mpwd"/"$2"_bash1.sh
 #!/bin/bash
 #SBATCH --time=00:10:00
 #SBATCH --mem=10G
@@ -556,18 +581,18 @@ cd "$mpwd"/$2/$picname
 
 #Decluster the image
 echo "Declustering image " $file #A shout-out to stdout what image it's currently working on, so you have an idea where in your set-up it is.
-python3 "$mpwd"/$2_python1.py -i "$file" -o "$mpwd"/$2/$picname -D print -v "$mpwd"/$2_variables.txt #Start the declustering of multi-plant pictures. The -D argument will ensure that also intermediary pictures of each processing step are saved. It is important to inspect these intermediary pictures after the pipeline is finished to get an idea of how the declustering behaved, and if parameters need tweaking.
+python3 "$mpwd"/$2_python1.py -i "$file" -o "$mpwd"/$2/$picname -D print -v "$mpwd"/notes.txt #Start the declustering of multi-plant pictures. The -D argument will ensure that also intermediary pictures of each processing step are saved. It is important to inspect these intermediary pictures after the pipeline is finished to get an idea of how the declustering behaved, and if parameters need tweaking.
 
 #And per declustered output file, measure the leaf area
 for file2 in "$mpwd/$2/$picname/$picname"*_mask.png
 do
-	python3 "$mpwd"/$2_python2.py -i "$file2" -D print -r "$mpwd"/$2/$picname.results.txt -v "$mpwd"/$2_variables.txt #Now analyze every single (supposed) plant from the declustered output
+	python3 "$mpwd"/$2_python2.py -i "$file2" -D print -r "$mpwd"/$2/$picname.results.txt -v "$mpwd"/notes.txt #Now analyze every single (supposed) plant from the declustered output
 done
 
 EOF
 
 #Second bash script that summarizes output and calls Python3
-cat <<\EOF > "$mpwd"/$2_bash2.sh
+cat <<\EOF > "$mpwd"/"$2"_bash2.sh
 #!/bin/bash
 #SBATCH --time=00:10:00
 #SBATCH --mem=10G
@@ -617,20 +642,23 @@ if [ -n "$(find "$mpwd/$1" -maxdepth 0 -type d -empty 2>/dev/null)" ]; then
 fi
 
 if ! [ -d "$mpwd/$2" ]; then
-	mkdir "$mpwd"/$2
+	mkdir "$mpwd"/"$2"
 fi
 
 #And now call all scripts per input picture and submit jobs
 if $hpc; then
-	for file in "$mpwd"/$1/*; do #Call all files that are in the specified input folder
-		sbatch "$mpwd"/$2_bash1.sh $1 $2 "$mpwd" "$file"
+	for file in "$mpwd"/"$1"/*; do #Call all files that are in the specified input folder
+		sbatch "$mpwd"/"$2"_bash1.sh "$1" "$2" "$mpwd" "$file"
 		sleep 0.5
 	done
-	sbatch --dependency=singleton --job-name=decluster "$mpwd"/$2_bash2.sh $1 $2 "$mpwd"
+	sbatch --dependency=singleton --job-name=decluster "$mpwd"/"$2"_bash2.sh "$1" "$2" "$mpwd"
 	watch squeue -u $USER
 else
-	for file in "$mpwd"/$1/*; do #Call all files that are in the specified input folder
-		bash "$mpwd"/$2_bash1.sh $1 $2 "$mpwd" "$file"
+	for file in "$mpwd"/"$1"/*; do #Call all files that are in the specified input folder
+		bash "$mpwd"/"$2"_bash1.sh "$1" "$2" "$mpwd" "$file"
 	done
-	bash "$mpwd"/$2_bash2.sh $1 $2 "$mpwd"
+	bash "$mpwd"/"$2"_bash2.sh "$1" "$2" "$mpwd"
 fi
+
+rm "$mpwd"/"$2"_bash*.sh
+rm "$mpwd"/"$2"_python*.sh
